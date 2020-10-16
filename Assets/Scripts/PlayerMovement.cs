1ce5a295afe406ca07ae4116f0af4662f5e40690
Damzwan <damian.vlaicu@gmail.com>
2020-10-16 13:22:17 +0200

    added simple 3d character movement/camera

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform camera;
+    
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+
+    private float turnSmoothVelocity;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+        var direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            var targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+            var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            
+            transform.rotation = Quaternion.Euler(0f, angle, 0);
+            var moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime); //deltaTime to make game frame rate independent
+        }
+    }
+}
