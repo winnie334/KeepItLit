8af69c337b69f5bf4d1bf9282678f1812898c46a
winand appels <winnie33winnie33@gmail.com>
2020-10-19 15:40:48 +0200

    improved movement, added collision, added fixed camera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,30 +5,56 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
-    public Transform camera;
+    public Transform mainCamera;
+    public Vector3 cameraOffset;
     
     public float speed = 6f;
     public float turnSmoothTime = 0.1f;
+    public float pushPower = 2f;
 
     private float turnSmoothVelocity;
 
+    private float gravity;
+
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        // First we move the controller down with gravity
+        gravity -= 9.81f * Time.deltaTime;
+        if (controller.isGrounded) gravity = 0;
+        controller.Move(new Vector3(0, gravity, 0) * Time.deltaTime);
+        
+        // Now we read the inputs and move our character accordingly
         var horizontal = Input.GetAxisRaw("Horizontal");
         var vertical = Input.GetAxisRaw("Vertical");
         var direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if (direction.magnitude >= 0.1f)
-        {
-            var targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+        if (direction.magnitude >= 0.1f) {
+            var targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
 
-            
             transform.rotation = Quaternion.Euler(0f, angle, 0);
             var moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime); //deltaTime to make game frame rate independent
         }
+
+        // Move the camera to a position above the player
+        mainCamera.transform.position = transform.position - cameraOffset;
+    }
+    
+    // If we run up against something with a rigidbody, we move it
+    void OnControllerColliderHit(ControllerColliderHit hit) {
+        Rigidbody body = hit.collider.attachedRigidbody;
+
+        // no rigidbody
+        if (body == null || body.isKinematic) return;
+
+        // We dont want to push objects below us
+        if (hit.moveDirection.y < -0.3) return;
+
+        // Calculate push direction from move direction,
+        // we only push objects to the sides never up and down
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        body.velocity = pushDir * pushPower;
     }
 }
