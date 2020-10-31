c9769111b362fa04ebf77a2eeb45413f241bea4f
ThibCL <thibault.cabourg@gmai.com>
2020-10-31 16:54:27 +0100

    review little cchange

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -1,21 +1,15 @@
-﻿    using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Fire : MonoBehaviour
-{
+public class Fire : MonoBehaviour {
     public float scaleFire;
-    public float speedDecreacing;
+    public float speedDreasing;
     public float minScale;
     public int densityFire;
     private ParticleSystem part;
     private ParticleSystem.ShapeModule sh;
     private ParticleSystem.EmissionModule em;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         part = this.GetComponent<ParticleSystem>();
         sh = part.shape;
         em = part.emission;
@@ -23,28 +17,22 @@ public class Fire : MonoBehaviour
         em.rateOverTime = (ParticleSystem.MinMaxCurve)(System.Math.Pow(scaleFire, 3) * densityFire);
     }
 
-    // Update is called once per frame
-     void Update()
-    {
+    void Update() {
         Collider[] woods = Physics.OverlapSphere(this.transform.position, sh.scale.x + 1);
-        foreach (var wood in woods)
-        {
-            if (!part.isStopped && wood.gameObject.GetComponent<Wood>())
-            {
+        foreach (var wood in woods) {
+            if (!part.isStopped && wood.gameObject.GetComponent<Wood>()) {
                 sh.scale += Vector3.ClampMagnitude(Vector3.one, wood.gameObject.GetComponent<Wood>().size);
                 em.rateOverTime = (ParticleSystem.MinMaxCurve)(System.Math.Pow(sh.scale.magnitude, 3));
                 Destroy(wood.gameObject);
             }
         }
 
-        if (!part.isStopped && sh.scale.magnitude < minScale)
-        {
+        if (!part.isStopped && sh.scale.magnitude < minScale) {
             part.Stop();
-        } else if (!part.isStopped)
-        {
-            sh.scale -= Vector3.ClampMagnitude(Vector3.one,speedDecreacing*Time.deltaTime);
+        } else if (!part.isStopped) {
+            sh.scale -= Vector3.ClampMagnitude(Vector3.one, speedDreasing * Time.deltaTime);
             em.rateOverTime = (ParticleSystem.MinMaxCurve)(System.Math.Pow(sh.scale.magnitude, 3) * densityFire);
         }
-         
+
     }
 }
