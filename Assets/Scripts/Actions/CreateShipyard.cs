27619c06719d9c92e0b0f2f30f2cb84b29f4cb5d
Damzwan <damian.vlaicu@gmail.com>
2020-11-27 07:27:30 +0100

    added barebones shipyard kit build constraint and replaced boat prefabs with newer model

--- Assets/Scripts/Actions/CreateShipyard.cs
+++ Assets/Scripts/Actions/CreateShipyard.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using Actions;
 using UnityEngine;
 
-public class CreateShipyard : MonoBehaviour, IAction {
+public class CreateShipyard : MonoBehaviour, IAction
+{
     public GameObject shipyard;
 
     public void execute(PlayerMovement player)
     {
         var position = transform.position;
-        Instantiate(shipyard, new Vector3(position.x + 1, position.y - 1.5f, position.z + 1),
-            Quaternion.identity);
+        var targetLoc = new Vector3(position.x + 1, position.y - 1.5f, position.z + 1);
+        if (targetLoc.y > 0) return;
+
+        Instantiate(shipyard, targetLoc, Quaternion.identity);
         player.removeObject(this.gameObject);
         Destroy(this.gameObject);
     }
