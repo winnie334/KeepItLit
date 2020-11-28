224eb3818cb5d0f2f026916d6beb932a20428b37
ThibCL <thibault.cabourg@gmai.com>
2020-11-28 15:37:33 +0100

    extract ressources

--- Assets/Scripts/Loot.cs
+++ Assets/Scripts/Loot.cs
@@ -2,19 +2,78 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Loot : MonoBehaviour {
-    public GameObject[] loot;
-    private bool isQuitting;
+[System.Serializable]
+public struct Loots {
+    public GameObject ressource;
+    public int number;
+}
 
-    private void OnApplicationQuit() {
-        isQuitting = true;
+public class Loot : MonoBehaviour {
+    public List<Item> tools;
+    public List<Loots> loots;
+    public int maxHealth;
+    public int stepNumber;
+
+    private int health;
+    private int step;
+    private int nextStep;
+    private int ressourcesByStep;
+
+    private void Start() {
+        health = maxHealth;
+        step = health / (stepNumber + 1);
+        nextStep = health - step;
+
+        var totalRessources = 0;
+        foreach (var loot in loots) {
+            totalRessources += loot.number;
+        }
+
+        ressourcesByStep = totalRessources / (stepNumber + 1);
     }
 
-    private void OnDestroy() {
-        if (isQuitting) return;
-        foreach (var item in loot) {
-            var spawnPos = transform.position + (Vector3) Random.insideUnitCircle + new Vector3(0, 2, 0);
-            Instantiate(item, spawnPos, Quaternion.identity);
+    public void Extract(Item item, int damage) {
+        if (!tools.Contains(item)) {
+            Debug.Log("Wrong tool");
+            return;
         }
+
+        health -= damage;
+        Debug.Log("Aie");
+
+        if (health <= 0) {
+            foreach (var loot in loots) {
+                for (var i = 0; i < loot.number; i++) {
+                    var spawnPos = transform.position + (Vector3)Random.insideUnitCircle + new Vector3(0, 2, 0);
+                    Instantiate(loot.ressource, spawnPos, Quaternion.identity);
+                }
+                Destroy(this.gameObject);
+            }
+        } else {
+            while (nextStep > health) {
+                nextStep -= step;
+                var ressourcesExtracted = 0;
+                while (ressourcesExtracted < ressourcesByStep) {
+                    var diff = ressourcesByStep - ressourcesExtracted;
+
+                    var loot = loots[loots.Count - 1];
+                    loots.RemoveAt(loots.Count - 1);
+
+                    if (loot.number > diff) {
+                        loot.number -= diff;
+                        loots.Add(loot);
+                    } else {
+                        diff = loot.number;
+                    }
+
+                    for (var i = 0; i < diff; i++) {
+                        var spawnPos = transform.position + (Vector3)Random.insideUnitCircle + new Vector3(0, 2, 0);
+                        Instantiate(loot.ressource, spawnPos, Quaternion.identity);
+                    }
+                    ressourcesExtracted += diff;
+                }
+            }
+        }
+
     }
 }
