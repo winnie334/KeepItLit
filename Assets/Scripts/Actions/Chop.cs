7fe28dfb4e594c750143a78b49456b0b4a1be186
ThibCL <43912845+ThibCL@users.noreply.github.com>
2020-11-29 16:41:00 +0100

    feature/extractRessources (#12)
    Add script to extract resources form tree, rock....

--- Assets/Scripts/Actions/Chop.cs
+++ Assets/Scripts/Actions/Chop.cs
@@ -2,15 +2,16 @@
 
 namespace Actions {
     public class Chop : MonoBehaviour, IAction {
-        
+
+        public int damage;
         public AudioSource audioSource;
-        
+
         public void execute(PlayerMovement playerMovement) {
             //TODO pick closest tree, or actually come up with a good way of detecting trees
             var hitCollider = Physics.OverlapSphere(transform.position + transform.rotation * Vector3.forward * 2, 2)[0];
-            if (!hitCollider.gameObject.CompareTag("Tree")) return;
-            Destroy(hitCollider.gameObject);
-            audioSource.Play();
+            if (!hitCollider.gameObject.CompareTag("Resource")) return;
+            hitCollider.gameObject.GetComponent<Loot>().Extract(gameObject.GetComponent<ItemAssociation>().item, damage);
+            //audioSource.Play();
         }
     }
 }
