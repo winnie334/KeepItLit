99ffaaebf8369da3ac407714a334105166184381
winand appels <winnie33winnie33@gmail.com>
2020-11-29 22:30:17 +0100

    added tutorial text, fix automatic crafting update

--- Assets/Scripts/Crafter.cs
+++ Assets/Scripts/Crafter.cs
@@ -6,27 +6,19 @@ using UnityEngine.Assertions;
 
 // Class responsible for detecting which items are available to craft, filtering possible recipes,
 // and doing the actual crafting of the new item
-public class Crafter : MonoBehaviour
-{
+public class Crafter : MonoBehaviour {
     public List<Recipe> knownRecipes;
     private float craftRadius = 6;
     private Dictionary<Item, List<GameObject>> availableItems;
     private CraftUI craftUI;
 
-    void Start()
-    {
+    void Start() {
         craftUI = GameObject.Find("Canvas").GetComponent<CraftUI>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
     }
 
     // Returns all available items nearby for crafting, along with their corresponding GameObjects 
     // (So we can remove the objects from the world if needed)
-    Dictionary<Item, List<GameObject>> getAvailableItems()
-    {
+    Dictionary<Item, List<GameObject>> getAvailableItems() {
         Collider[] nearbyItems = Physics.OverlapSphere(transform.position, craftRadius) // Look for objects in a radius
             .Where(hit => hit.GetComponent<ItemAssociation>() != null)
             .ToArray(); // If it doesn't have item script, it isn't an item
@@ -34,8 +26,7 @@ public class Crafter : MonoBehaviour
         // We categorize all nearby objects by item. Basically a lookup table for each item to the GameObjects
         // e.g. 'wood' -> [GameObject3, GameObject4], 'stone' -> [GameObject2], etc
         var availableItems = new Dictionary<Item, List<GameObject>>();
-        foreach (var obj in nearbyItems)
-        {
+        foreach (var obj in nearbyItems) {
             var item = obj.GetComponent<ItemAssociation>().item;
             if (!availableItems.ContainsKey(item)) availableItems.Add(item, new List<GameObject> {obj.gameObject});
             else availableItems[item].Add(obj.gameObject);
@@ -45,49 +36,39 @@ public class Crafter : MonoBehaviour
     }
 
     // Gives a list of recipes the player has the necessary materials for
-    public List<Recipe> getPossibleRecipes()
-    {
+    public List<Recipe> getPossibleRecipes() {
         if (availableItems is null) availableItems = getAvailableItems();
         return knownRecipes.Where(recipe => canMakeRecipe(recipe, availableItems)).ToList();
     }
 
-    void destroyRequiredMaterials(List<Item> requiredMaterials)
-    {
+    void destroyRequiredMaterials(List<Item> requiredMaterials) {
         var availableItems = getAvailableItems();
         // Assert.IsTrue(canMakeRecipe(recipe, availableItems)); //TODO put back on
-        foreach (var requiredMaterial in requiredMaterials)
-        {
-            var bestObject =
-                availableItems[
-                    requiredMaterial][
-                    0]; // If you want to do smart picking (closest first, direct raycast, ...) do it here
+        foreach (var requiredMaterial in requiredMaterials) {
+            var bestObject = availableItems[requiredMaterial][0]; // If you want to do smart picking (closest first, direct raycast, ...) do it here
             availableItems[requiredMaterial].Remove(bestObject);
             Destroy(bestObject);
         }
     }
 
     // Executes a recipe by removing the ingredients from the world and spawning the outcome of the recipe
-    public void craftPlayerRecipe(Recipe recipe)
-    {
+    public void craftPlayerRecipe(Recipe recipe) {
         // maybe save available items? Has some design consequences, todo discuss
         destroyRequiredMaterials(recipe.requiredItems);
         Instantiate(recipe.resultingItem, transform.position + transform.rotation * Vector3.forward,
             Quaternion.identity);
     }
 
-    public void craftShipyardRecipe(Recipe recipe, GameObject shipyard)
-    {
+    public void craftShipyardRecipe(Recipe recipe, GameObject shipyard) {
         destroyRequiredMaterials(recipe.requiredItems);
         shipyard.GetComponent<Shipyard>().handleCreateRecipe(recipe);
     }
 
     // Returns true if the given recipe can be made with a given list of items
-    bool canMakeRecipe(Recipe recipe, Dictionary<Item, List<GameObject>> availableItems)
-    {
+    bool canMakeRecipe(Recipe recipe, Dictionary<Item, List<GameObject>> availableItems) {
         // We create a new copy (prevent pass-by-reference errors!!) and change the list of objects to a simple count
         var availableCount = availableItems.ToDictionary(entry => entry.Key, entry => entry.Value.Count);
-        foreach (var requiredItem in recipe.requiredItems)
-        {
+        foreach (var requiredItem in recipe.requiredItems) {
             if (!availableCount.ContainsKey(requiredItem) || availableCount[requiredItem] <= 0) return false;
             availableCount[requiredItem]--;
         }
@@ -96,21 +77,18 @@ public class Crafter : MonoBehaviour
     }
 
     // Returns the amount of necessary items for a given recipe and how many of those items are available
-    public Dictionary<Item, Tuple<int, int>> getNeededItems(Recipe recipe)
-    {
-        var availableCount = getAvailableItems().ToDictionary(entry => entry.Key, entry => entry.Value.Count);
+    public Dictionary<Item, Tuple<int, int>> getNeededItems(Recipe recipe) {
+        var availableCount = availableItems.ToDictionary(entry => entry.Key, entry => entry.Value.Count);
         var requiredCount = recipe.requiredItems.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
         return requiredCount.Keys.ToDictionary(item => item, item =>
             new Tuple<int, int>(availableCount.GetOrDef(item), requiredCount[item]));
     }
 
-    public void resetAvailableItems()
-    {
+    public void resetAvailableItems() {
         availableItems = new Dictionary<Item, List<GameObject>>();
     }
 
-    private void OnTriggerEnter(Collider hit)
-    {
+    private void OnTriggerEnter(Collider hit) {
         if (availableItems is null) return;
         var itemAssociation = hit.gameObject.GetComponent<ItemAssociation>();
         if (itemAssociation is null) return;
@@ -120,8 +98,7 @@ public class Crafter : MonoBehaviour
         craftUI.refreshUI();
     }
 
-    private void OnTriggerExit(Collider hit)
-    {
+    private void OnTriggerExit(Collider hit) {
         if (availableItems is null) return;
         var itemAssociation = hit.gameObject.GetComponent<ItemAssociation>();
         if (itemAssociation is null) return;
