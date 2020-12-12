aa09a06006a830e181d4b49db93229641425c36a
Damzwan <damian.vlaicu@gmail.com>
2020-12-12 16:55:59 +0100

    added fishing

--- Assets/Scripts/Actions/Fish.cs
+++ Assets/Scripts/Actions/Fish.cs
@@ -1,22 +1,42 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using UnityEngine;
 
 namespace Actions {
     public class Fish : MonoBehaviour, IAction {
 
         public GameObject FishObject;
+        public GameObject Bobber;
+        
         public int durability = 3;
-
+        private GameObject currBobber;
+        
         public void execute(PlayerMovement playerMovement) {
-            if (playerMovement.transform.position.y < 0) {
-                var pos = transform.position;
-                Instantiate(FishObject, new Vector3(pos.x, pos.y + 2, pos.z), Quaternion.identity);
-                durability--;
-                if (durability == 0) {
-                    playerMovement.releaseObjects();
-                    Destroy(this.gameObject);
+            if (currBobber) {
+                if (currBobber.GetComponent<Bobber>().hasFish()) {
+                    var boberPos = currBobber.transform.position;
+                    var fish = Instantiate(FishObject, boberPos, Quaternion.identity);
+                    var targetDir = (transform.position - boberPos).normalized;
+                    fish.GetComponent<Rigidbody>().AddForce(new Vector3(targetDir.x * 1000, targetDir.y*2000, targetDir.z * 1000));
+                    durability--;
+                    if (durability == 0) {
+                        playerMovement.releaseObjects();
+                        Destroy(gameObject);
+                    }
+                    Destroy(currBobber);
+                }
+                else {
+                    Destroy(currBobber);
+                    throwBobber(playerMovement.transform.forward);
                 }
             }
+            else throwBobber(playerMovement.transform.forward);
+        }
+
+        void throwBobber(Vector3 dir) {
+            var pos = transform.position;
+            currBobber = Instantiate(Bobber, new Vector3(pos.x, pos.y + 0.2f, pos.z), Quaternion.identity);
+            currBobber.GetComponent<Rigidbody>().AddForce(dir*1000);
         }
     }
 }
---
+++ Assets/Scripts/Bobber.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Bobber : MonoBehaviour {
+
+    public int minWaitTime = 1;
+    public int maxWaitTime = 6;
+
+    private bool triggerDisabled;
+    private bool inSea;
+
+    private bool shouldDrop = true;
+    private bool isDropped;
+
+    private Transform seaTransform;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        seaTransform = GameObject.Find("Sea").transform;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (inSea) {
+            var pos = transform.position;
+            if (!isDropped) transform.position = new Vector3(pos.x, seaTransform.position.y, pos.z);
+            if (shouldDrop) StartCoroutine(dropDobber());
+        }
+        else if (transform.position.y <= seaTransform.position.y) {
+            rb.useGravity = false;
+            rb.velocity = Vector3.zero;
+            transform.rotation = Quaternion.identity;
+            inSea = true;
+        }
+    }
+
+    public bool hasFish() {
+        return isDropped;
+    }
+
+    IEnumerator dropDobber() {
+        shouldDrop = false;
+        float waitTime = Random.Range (minWaitTime, maxWaitTime);
+        yield return new WaitForSeconds (waitTime);
+        isDropped = true;
+        rb.AddForce(new Vector3(0, -50f, 0));
+        StartCoroutine(liftDobber());
+    }
+    
+    IEnumerator liftDobber() {
+        yield return new WaitForSeconds (1);
+        shouldDrop = true;
+        isDropped = false;
+    }
+    
+
+    private void OnTriggerExit(Collider other) {
+        if (triggerDisabled) return;
+        GetComponent<Collider>().isTrigger = false;
+        triggerDisabled = true;
+    }
+}
