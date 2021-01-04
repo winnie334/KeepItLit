56f92096fc2c66ea321c23df9368138b74e8c851
Damzwan <damian.vlaicu@gmail.com>
2021-01-04 18:21:53 +0100

    added fade out

---
+++ Assets/Scripts/BlackFadeOut.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
+
+public class BlackFadeOut : MonoBehaviour {
+
+    private UnityEngine.UI.Image img;
+    private float timer;
+    public float fadeOutDuration;
+
+    // Start is called before the first frame update
+    void Start() {
+        img = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (timer > fadeOutDuration) return;
+        timer += Time.deltaTime;
+        img.color = new Color(0, 0, 0, Mathf.Lerp(1, 0, timer / fadeOutDuration));
+    }
+}
